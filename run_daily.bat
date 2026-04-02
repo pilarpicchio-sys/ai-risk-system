8a67e241121a6c0fac4943efed979d1d5ce132bf
Gianluca <pilarpicchio@gmail.com>
2026-04-02 23:03:01 +0200

    auto update

--- run_daily.bat
+++ run_daily.bat
@@ -7,4 +7,9 @@ call venv\Scripts\python.exe -m src.app.run_live
 echo === BACKTEST ===
 call venv\Scripts\python.exe -m src.portfolio.multi_asset_backtest
 
+echo === GIT PUSH ===
+git add .
+git commit -m "auto update"
+git push
+
 echo === DONE ===
